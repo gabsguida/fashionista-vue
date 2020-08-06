b50018a4f9cd9d0f75fdf0ee94320087683a1631
gabsguida <gabrielaguida7@gmail.com>
2020-08-06 11:16:21 -0300

    FAS-12 & FAS-1 & FAS-10 creating Sidebar-Navbar and folders

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="sidebar__content">
+        <div :class="[className, (isRight ? 'sidebar--right' : '')]" class="sidebar" data-testid="sidebar">
+            <div class="sidebar__header">
+                <span class="sidebar__header-title">{{ title }}</span>
+                
+            </div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Sidebar extends Vue {
+  // adding '!' tells to typescript to 'relax' cause someone else is going to assign this property a value
+  @Prop({ required: true, type: Boolean, default: false }) isRight!: boolean;
+  @Prop({ required: true, type: String, default: "" }) className!: string;
+  @Prop({ required: true, type: String, default: "" }) title!: string;
+}
+</script>
---
+++ src/layouts/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Home</p>
+    <vs-button>Click me!</vs-button>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {}
+</script>
