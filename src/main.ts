4888be15a843bbdd796a0b72b9aceaaea2c7c647
gabsguida <gabrielaguida7@gmail.com>
2020-08-06 11:15:18 -0300

    adding vuesax as global

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
 
+import Vuesax from "vuesax";
+import "material-icons/iconfont/material-icons.scss"; //Material Icons
+import "vuesax/dist/vuesax.css"; // Vuesax
+Vue.use(Vuesax);
+
 Vue.config.productionTip = false;
 
 new Vue({
