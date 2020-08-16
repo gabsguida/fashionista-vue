4ff689d34df109e694e416030a32ca7c012b3f44
gabsguida <gabrielaguida7@gmail.com>
2020-08-15 22:53:50 -0300

    creating Navbar component - WIP- FAS-13

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <vs-navbar v-model="active">
+      <template #left>
+        <div className="header__logo">
+          <router-link to="/" title="Ir para a página inicial">
+            <img
+              src="img/Logo.png"
+              alt="Fashionista"
+              className="header__logo-img"
+            />
+          </router-link>
+        </div>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+</script>
