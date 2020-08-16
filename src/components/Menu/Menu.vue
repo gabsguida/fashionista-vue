c8c22c532f6ba9298e96388140b71138dc43af43
gabsguida <gabrielaguida7@gmail.com>
2020-08-15 22:52:23 -0300

    creating Menu component - WIP - FAS-3

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="menu__nav" data-testid="menu">
+    <ul v-for="(links, index) in menuLinks" :key="index">
+      <li>
+        <router-link :to="links.url">{{ links.title }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Menu extends Vue {
+  private menuLinks: Array<object> = [
+    {
+      url: "/",
+      title: "Página Inicial"
+    },
+    {
+      url: "/",
+      title: "Promoções"
+    },
+    {
+      url: "/",
+      title: "Blusas"
+    },
+    {
+      url: "/",
+      title: "Calças"
+    },
+    {
+      url: "/",
+      title: "Vestidos"
+    }
+  ];
+}
+</script>
