ea1c6bc649551159ee2e06cc3732cda0f07c1d4e
gabsguida <gabrielaguida7@gmail.com>
2020-08-18 16:39:22 -0300

    fix eslint problem

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -2,7 +2,9 @@
   <nav class="menu__nav" data-testid="menu">
     <ul v-for="(links, index) in menuLinks" :key="index">
       <li>
-        <router-link :to="links.url">{{ links.title }}</router-link>
+        <router-link class="menu__nav-link" :to="links.url">
+          {{ links.title }}
+        </router-link>
       </li>
     </ul>
   </nav>
@@ -37,3 +39,31 @@ export default class Menu extends Vue {
   ];
 }
 </script>
+
+<style scoped>
+.menu__container {
+  background-color: #212121;
+  color: #fff;
+}
+
+.menu__container .menu__nav ul li .menu__nav-link {
+  display: block;
+  padding: 15px;
+  border-bottom: 1px solid #555;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-weight: bold;
+  color: #fff;
+  transition: all 0.2s ease-in-out;
+}
+
+.menu__container .menu__nav ul li:first-child .menu__nav-link {
+  border-top: 1px solid #555;
+}
+
+.menu__container .menu__nav ul li .menu__nav-link:hover {
+  color: #b99646;
+  transition: all 0.2s ease-in-out;
+  padding-left: 25px;
+}
+</style>
