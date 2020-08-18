85c3b7e74904fedac0a387c5c6fadfcce67896b5
gabsguida <gabrielaguida7@gmail.com>
2020-08-18 16:53:32 -0300

    creating router - FAS-8

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+
+import router from './router'
 import store from "./store";
 
 import Vuesax from "vuesax";
+Vue.use(Vuesax);
+
 import "material-icons/iconfont/material-icons.scss"; //Material Icons
 import "vuesax/dist/vuesax.css"; // Vuesax
-Vue.use(Vuesax);
+import "./assets/scss/main.scss"; 
+
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
