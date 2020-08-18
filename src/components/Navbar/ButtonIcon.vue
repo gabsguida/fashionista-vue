4ce69da8287292308e9c7b7357b4b8dceb82dd6e
gabsguida <gabrielaguida7@gmail.com>
2020-08-18 16:36:30 -0300

    adding button icons - WIP - FAS-13

---
+++ src/components/Navbar/ButtonIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="className">
+    <a
+      href="/"
+      class="header__icon sidebar-trigger"
+      :data-sidebar="sidebar"
+      :data-badge="badge"
+    >
+      <span class="material-icons">{{ iconName }}</span>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class ButtonIcon extends Vue {
+  // adding '!' tells to typescript to 'relax' cause someone else is going to assign this property a value
+  @Prop({ required: true, type: String }) className!: string;
+  @Prop({ required: true, type: String }) sidebar!: string;
+  @Prop({ type: String }) badge!: string;
+  @Prop({ required: true, type: String }) iconName!: string;
+}
+</script>
+
+<style scoped>
+.header__icon .sidebar-trigger {
+  cursor: pointer;
+}
+</style>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,22 +1,113 @@
 <template>
-  <div>
-    <vs-navbar v-model="active">
-      <template #left>
-        <div className="header__logo">
-          <router-link to="/" title="Ir para a página inicial">
-            <img
-              src="img/Logo.png"
-              alt="Fashionista"
-              className="header__logo-img"
-            />
-          </router-link>
+  <header class="header" data-testid="header">
+    <div class="header__container">
+      <ButtonIcon
+        iconName="menu"
+        sidebar=".menu__container"
+        className="header__hamburguer"
+      />
+      <div class="header__logo">
+        <a to="/" title="Ir para a página inicial">
+          <img
+            src="../../assets/images/Logo.png"
+            alt="Fashionista"
+            class="header__logo-img"
+          />
+        </a>
+      </div>
+      <div class="header__actions">
+        <div class="header__actions-list">
+          <ButtonIcon
+            className="header__actions-search"
+            iconName="search"
+            sidebar=".search__container"
+          />
+          <ButtonIcon
+            className="header__actions-cart"
+            iconName="shopping_cart"
+            sidebar=".cart__container"
+            badge="1"
+          />
+          <ButtonIcon
+            className="header__actions-account"
+            iconName="account_circle"
+            sidebar=".account__container"
+          />
         </div>
-      </template>
-    </vs-navbar>
-  </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import ButtonIcon from "./ButtonIcon.vue";
 
+@Component({
+  components: {
+    ButtonIcon
+  }
+})
+export default class Navbar extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+  background-color: #ffffff;
+}
+
+.header__container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  height: 60px;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+}
+
+.header__logo-img {
+  display: block;
+  min-width: 150px;
+  width: 80%;
+  cursor: pointer;
+}
+
+.header__icon {
+  font-size: 2rem;
+  padding: 5px 10px 10px;
+  position: relative;
+  height: 40px;
+  display: inline-block;
+}
+
+.header__actions-cart {
+  position: relative;
+}
+
+.header__icon[data-badge]:not([data-badge="0"]):before {
+  content: attr(data-badge);
+  display: inline-block;
+  height: 5px;
+  background: #b99646;
+  position: absolute;
+  top: 2px;
+  font-size: 0.9rem;
+  padding: 8px;
+  border-radius: 50%;
+  min-width: 6px;
+  line-height: 0.4rem;
+  text-align: center;
+  color: white;
+  right: 3px;
+  z-index: 5;
+}
+
+.header__actions-list {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
