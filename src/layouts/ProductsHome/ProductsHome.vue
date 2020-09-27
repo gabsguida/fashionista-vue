0f5989989fa69abc8b8acf844f1086e42bcc36df
gabsguida <gabrielaguida7@gmail.com>
2020-09-26 22:13:51 -0300

    adding breakpoint to products cards - FAS-23

--- src/layouts/ProductsHome/ProductsHome.vue
+++ src/layouts/ProductsHome/ProductsHome.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="products__container">
-    <h2>Nossos Produtos</h2>
-    <template v-for="(product, index) in productsList">
-      <CustomCard
+  <div class="products__home">
+    <h2 class="products__home-title">Nossos Produtos</h2>
+    <div class="products__container">
+      <div
+        v-for="(product, index) in productsList"
         :key="index"
-        :productName="product.name"
-        :productImg="product.image"
-        :productPriceSale="product.actual_price"
-        :productRegularPrice="product.regular_price"
-        :isOnSale="product.on_sale"
-        :discountPercentage="product.discount_percentage"
-      />
-    </template>
+        class="product__container"
+      >
+        <CustomCard
+          :key="index"
+          :productName="product.name"
+          :productImg="product.image"
+          :productPriceSale="product.actual_price"
+          :productRegularPrice="product.regular_price"
+          :isOnSale="product.on_sale"
+          :discountPercentage="product.discount_percentage"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,7 +28,7 @@ import { Component, Vue } from "vue-property-decorator";
 
 import products from "@/assets/mocks/products.json";
 
-import CustomCard from "@/components/ProductCard/ProductCard.vue";
+import CustomCard from "@/components/CustomCard/CustomCard.vue";
 
 @Component({
   components: {
@@ -36,10 +42,42 @@ export default class ProductsHome extends Vue {
 </script>
 
 <style lang="scss" scoped>
+// defining a global mixin
+@mixin globalbreak($screen) {
+  @media (min-width: $screen+"px") {
+    @content;
+  }
+}
+
+.products__home-title {
+  margin: 0;
+  padding: 10px 5px;
+
+  @include globalbreak(768) {
+    padding: 10px 12px;
+  }
+}
+
 .products__container {
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-content: center;
+
+  @include globalbreak(768) {
+    grid-template-columns: repeat(3, minmax(200px, auto));
+    display: grid;
+    gap: 20px;
+  }
+
+  @include globalbreak(1024) {
+    grid-template-columns: repeat(4, minmax(200px, auto));
+    gap: 15px;
+  }
+
+  @include globalbreak(1440) {
+    grid-template-columns: repeat(5, minmax(200px, auto));
+    gap: 50px;
+  }
 }
 </style>
